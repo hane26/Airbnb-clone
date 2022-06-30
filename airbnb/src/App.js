e0dff040da4a94d0161d0bc49804fcea91103b3f
hane <adrplusrayane@hotmail.com>
2022-06-30 12:28:44 +0100

    added the starter project

---
+++ airbnb/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     <h2>
+      This is just a test 
+     </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+///        <img src={logo} className="App-logo" alt="logo" />
